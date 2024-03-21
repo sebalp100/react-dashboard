60c1bf28d854e4481e5465923fd132dbaa17a0a3
sebalp100 <sebalp100@gmail.com>
2024-03-21 18:11:29 -0300

    Fix jsconfig

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import { createTheme } from '@mui/material/styles';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { themeSettings } from 'theme';
+import Layout from './scenes/layout';
+import Dashboard from './scenes/dashboard';
+import { themeSettings } from './theme';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
